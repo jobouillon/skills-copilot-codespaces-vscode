66eb9d7937a0e30e0a795e04f1fed0b2bc1726db
jobouillon <90056299+jobouillon@users.noreply.github.com>
2024-12-29 00:11:20 +0000

    add file

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+  // Create a new member object
+  var member = {
+    name: 'John Doe',
+    skills: ['JavaScript', 'HTML', 'CSS'],
+    age: 25
+  };
+
+  // Return the object
+  return member;
+}
