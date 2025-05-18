ac7fef8a1ef496f995c295183e6fc65d7f29afe2
Jonatan Bouillon <90056299+jobouillon@users.noreply.github.com>
2025-05-17 22:13:59 -0400

    Update member.js
    Add properties.

--- member.js
+++ member.js
@@ -3,7 +3,9 @@ function skillsMember() {
   var member = {
     name: 'John Doe',
     skills: ['JavaScript', 'HTML', 'CSS'],
-    age: 25
+    age: 25,
+    heitgh: 180,
+    weigth; 300
   };
 
   // Return the object
